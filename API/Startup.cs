c2df97e1a17f2a68d3037c9d1749982f0820b84a
leerobb <76053808+leerobb@users.noreply.github.com>
2022-05-05 10:42:51 -0400

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -43,7 +43,13 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-            
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
             
         }
 
@@ -60,6 +66,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
